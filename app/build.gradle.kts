58767daa7fff21771a90904cbbb1c9ec7caf5f92
vasanto2025 <vasanti.k@loyalstring.com>
2025-11-07 16:20:36 +0530

    version 1.35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.loyalstring.rfid"
         minSdk = 26
         targetSdk = 35
-        versionCode = 34
-        versionName = "1.34"
+        versionCode = 35
+        versionName = "1.35"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/com/loyalstring/rfid/viewmodel/ProductListViewModel.kt
+++ app/src/main/java/com/loyalstring/rfid/viewmodel/ProductListViewModel.kt
@@ -1,10 +1,14 @@
 package com.loyalstring.rfid.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.loyalstring.rfid.data.local.entity.BulkItem
 import com.loyalstring.rfid.repository.BulkRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -12,6 +16,21 @@ class ProductListViewModel @Inject constructor(
     repository: BulkRepository
 ) : ViewModel() {
 
-    val productList: Flow<List<BulkItem>> = repository.getAllBulkItems()
+  //  val productList: Flow<List<BulkItem>> = repository.getAllBulkItems()
+
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading: StateFlow<Boolean> = _isLoading
+
+    private val _productList = MutableStateFlow<List<BulkItem>>(emptyList())
+    val productList: StateFlow<List<BulkItem>> = _productList
+
+    init {
+        viewModelScope.launch {
+            repository.getAllBulkItems().collect { items ->
+                _productList.value = items
+                _isLoading.value = false
+            }
+        }
+    }
 
 }

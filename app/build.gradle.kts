5de6d1abd6a588e0d39417383e40298b4b69ee40
vasanto2025 <vasanti.k@loyalstring.com>
2026-06-19 11:48:32 +0530

    version 2.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.loyalstring.rfid"
         minSdk = 26
         targetSdk = 36
-       versionCode = 111
-        versionName = "2.9"
+       versionCode = 112
+        versionName = "2.10"
         /*versionCode = 8
         versionName = "1.8"*/
 
@@ -31,7 +31,7 @@ android {
     buildTypes {
         debug {
             signingConfig = signingConfigs.getByName("debug")
-            isDebuggable = true
+            isDebuggable = false
             isMinifyEnabled = false
         }
         
